8ce3f75d1c15e482b3b924fa60eb5644f2787af4
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-02-03 18:01:55 +0530

    [Prashant]Add: Creating new Contacts

---
+++ AddressBookProgram/AddressBookProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AddressBookProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+        }
+    }
+}
