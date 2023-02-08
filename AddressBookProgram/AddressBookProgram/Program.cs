2c08d8783009785e6b1fb92a7f92b4befcf8c397
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-02-08 16:49:08 +0530

    [Prashant]Add: Ability to add a new contacts

--- AddressBookProgram/AddressBookProgram/Program.cs
+++ AddressBookProgram/AddressBookProgram/Program.cs
@@ -6,6 +6,27 @@ namespace AddressBookProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+            Person person = new Person();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Address Book Program");
+                Console.WriteLine("Select Option:\n" +
+                    "1) Find the Create Contacts\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                
+                switch (option)
+                {
+                    case 1:
+                        Person.CreateContacts();
+                        break;
+
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
+            }
         }
     }
 }
