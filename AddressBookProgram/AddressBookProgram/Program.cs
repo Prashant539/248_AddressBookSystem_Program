4303532ef2f9f1ca11e032e9215d19c4a908e751
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-02-08 16:59:05 +0530

    [Prashant]Add: Ability to edit persons details

--- AddressBookProgram/AddressBookProgram/Program.cs
+++ AddressBookProgram/AddressBookProgram/Program.cs
@@ -12,7 +12,9 @@ namespace AddressBookProgram
             {
                 Console.WriteLine("Welcome to Address Book Program");
                 Console.WriteLine("Select Option:\n" +
-                    "1) Find the Create Contacts\n");
+                    "1) Find the Create Contacts\n" +
+                    "2) Find the Display of Contacts\n" +
+                    "3) Find the edit contacts\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
                 
@@ -20,6 +22,13 @@ namespace AddressBookProgram
                 {
                     case 1:
                         Person.CreateContacts();
+                        break;
+                    case 2:
+                        Person.Display();
+                        break;
+
+                    case 3:
+                        Person.editContact();
                         break;
 
                     default:
