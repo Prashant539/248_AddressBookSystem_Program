76fb68f3502c9318483205e8d1a23eff4fdfcdf5
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-02 19:23:54 +0530

    [Prashant]Add: Ability to search a person in City

--- AddressBookProgram/AddressBookProgram/Person.cs
+++ AddressBookProgram/AddressBookProgram/Person.cs
@@ -178,7 +178,22 @@ namespace AddressBookProgram
             else
             {
                 Console.WriteLine("Contact is not present");
-                
+
+            }
+        }
+        public void SearchPersonByCity()
+        {
+            Console.WriteLine("enter the name to search: ");
+            string personName = Console.ReadLine();
+            Console.WriteLine("enter the city to search: ");
+            string cityname = Console.ReadLine();
+            foreach (var pair in book.Keys)
+            {
+                Console.WriteLine("Address Book Name " + pair);
+                foreach (var data in person.FindAll(e => e.City == cityname && e.FName == personName))
+                {
+                    Console.WriteLine("The Contact Details of " + data.City + " are\n:" + data.FName + " " + data.LName + " " + data.Zipcode + " " + data.PhoneNumber);
+                }
             }
         }
     }
